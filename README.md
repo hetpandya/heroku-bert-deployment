bd35cf19d9c12e9414f22c497da80725aabb4ca1
root <themlhorizons@gmail.com>
2020-10-19 06:02:26 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Deploying BERT on Heroku
+
+Code for the medium blog post [Deploying BERT on Heroku](https://pages.github.com/).
+
+## Local Deployment
+### Requirements
+
+Install the required libraries using
+
+`pip install -r requirements.txt`
+
+### Inference
+Run the server using
+
+`python app.py`
+
+Then go this url
+http://localhost:5000
+
+### Screenshot
+![Screenshot](https://github.com/thehetpandya/heroku-bert-deployment/blob/master/images/screenshot.PNG?raw=true)
+
+### License
+Please check `LICENSE` for more details.
---
+++ app.py
@@ -0,0 +1,80 @@
+import os
+from flask import Flask, render_template
+from flask import request
+
+
+import torch
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+
+# name = "twmkn9/albert-base-v2-squad2"
+# name = "distilbert-base-uncased-distilled-squad"
+name = "mrm8488/bert-small-finetuned-squadv2"
+
+tokenizer = AutoTokenizer.from_pretrained(name,)
+
+model = AutoModelForQuestionAnswering.from_pretrained(name)
+
+def answer_question(question, answer_text):
+    '''
+    Takes a `question` string and an `answer` string and tries to identify 
+    the words within the `answer` that can answer the question. Prints them out.
+    '''
+    
+    # tokenize the input text and get the corresponding indices
+    token_indices = tokenizer.encode(question, answer_text)
+
+    # Search the input_indices for the first instance of the `[SEP]` token.
+    sep_index = token_indices.index(tokenizer.sep_token_id)
+
+    seg_one = sep_index + 1
+
+    # The remainders lie in the second segment.
+    seg_two = len(token_indices) - seg_one
+    
+    # Construct the list of 0s and 1s.
+    segment_ids = [0]*seg_one + [1]*seg_two
+
+    # get the answer for the question
+    start_scores, end_scores = model(torch.tensor([token_indices]), # The tokens representing our input combining question and answer.
+                                    token_type_ids=torch.tensor([segment_ids])) # The segment IDs to differentiate question from answer
+
+    # Find the tokens with the highest `start` and `end` scores.
+    answer_begin = torch.argmax(start_scores)
+    answer_end = torch.argmax(end_scores)
+
+    # Get the string versions of the input tokens.
+    indices_tokens = tokenizer.convert_ids_to_tokens(token_indices)
+    
+    answer = indices_tokens[answer_begin:answer_end+1]
+    #remove special tokens
+    answer = [word.replace("▁","") if word.startswith("▁") else word for word in answer] #use this when using model "twmkn9/albert-base-v2-squad2"
+    answer = " ".join(answer).replace("[CLS]","").replace("[SEP]","").replace(" ##","")
+    
+    return answer
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  
+    if request.method == 'POST':
+      form = request.form
+      result = []
+      bert_abstract = form['paragraph']
+      question = form['question']
+      result.append(form['question'])
+      result.append(answer_question(question, bert_abstract))
+      result.append(form['paragraph'])
+
+      return render_template("index.html",result = result)
+
+    # answer = answer_question(question, bert_abstract)
+
+    # return answer
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
