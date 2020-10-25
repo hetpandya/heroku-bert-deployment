31adcd5c14e1e1f57711accb417ed2114141bfd4
ML Horizons <themlhorizons@gmail.com>
2020-10-25 19:57:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import request
 import torch
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
-name = "twmkn9/albert-base-v2-squad2"
+# name = "twmkn9/albert-base-v2-squad2"
 # name = "distilbert-base-uncased-distilled-squad"
 #name = "mrm8488/bert-small-finetuned-squadv2"
 
